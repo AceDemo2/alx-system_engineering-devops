0c15c2153ea6fe535bf6261b1a752325679cd619
AceDemo2 <lawal641@gmail.com>
2024-06-20 16:20:05 -0700

    manage proceses

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,2 +1,32 @@
-#!/usr/bin/env bash
-#
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * infinite_while - runs an infinite loop with a sleep of 1 second in each iteration.
+ * Return: 0 always
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+/**
+ * main - creates 5 zombie processes.
+ * Return: 0 on success
+ */
+int main()
+{
+	int i;
+	pid_t j;
+	
+	for (i = 0; i < 5; i++)
+	{
+		j = fork();
+		if (i == 0)
+			exit(0);
+	}
+	infinite_while();
+}
