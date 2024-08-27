b9904f0f18b80131a905724e82bbc3c81c98d183
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-27 16:29:01 -0700

    gatherAPI

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com'
     response = requests.get(url + '/users/' + sys.argv[1]).json()
-    tasks = requests.get(url + '/todos', params={'userId':sys.argv[1]}).json()
+    tasks = requests.get(url + '/todos', params={'userId': sys.argv[1]}).json()
     name = response.get('name')
     content = []
     for i in tasks:
