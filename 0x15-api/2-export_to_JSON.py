b10da7deb185c26fe0b74ede3438aa49b115964b
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-27 18:50:39 -0700

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""export to json"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    usrid = sys.argv[1]
+    response = requests.get(url + '/users/' + usrid).json()
+    tasks = requests.get(url + '/todos', params={'userId': sys.argv[1]}).json()
+    name = response.get('username')
+    with open(f'{usrid}.json', 'w') as jsonf:
+        dic = {'usrid': []}
+        for task in tasks:
+            tstatus = task.get('completed')
+            title = task.get('title')
+            dic[usrid].append({'task': title, 'completed': tstatus, 'username': name})
+        json.dump(dic, jsonf)
