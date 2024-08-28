8dfaef9efff4821b78c6464702ca9d966685eb2c
AceDemo2 <lawaladepoju705@gmail.com>
2024-08-27 19:31:49 -0700

    export to json dic

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,21 +7,18 @@ import requests
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com'
     usrid = 1
+    dic = {}
     while True:
-        response = requests.get(url + '/users/' + usrid).json()
+        response = requests.get(url + '/users/' + str(usrid)).json()
         if not response:
             break
         tasks = requests.get(url + '/todos', params={'userId': usrid}).json()
         name = response.get('username')
-        dic = {str(usrid): []}
         for task in tasks:
             tstatus = task.get('completed')
             title = task.get('title')
-            dic[str(usrid)].append(
-                    {'username': name,
-                        'task': title,
-                        'completed': tstatus,
-                        })
-        with open(f'{usrid}.json', 'w') as jsonf:
-            json.dump(dic, jsonf)
-    usrid += 1
+            dic[str(usrid)] = [{'username': name, 'task': title, 'completed': tstatus}]
+        
+        usrid += 1
+    with open(f'{usrid}.json', 'w') as jsonf:
+        json.dump(dic, jsonf)
