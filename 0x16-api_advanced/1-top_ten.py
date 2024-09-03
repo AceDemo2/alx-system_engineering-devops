1316fe14875bf5cb4c656d59917d96c433ffecc3
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-02 19:00:34 -0700

    print title

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+Prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 
 from requests import get
@@ -9,25 +9,38 @@ from requests import get
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
     """
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return  # Exit early if invalid input
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
-    results = response.json()
+
+    # Check if the request was successful
+    if response.status_code != 200:
+        print("None")
+        return
 
     try:
-        my_data = results.get('data').get('children')
+        results = response.json()
+        my_data = results.get('data', {}).get('children', [])
+
+        if not my_data:
+            print("None")
+            return
 
         for i in my_data:
-            print(i.get('data').get('title'))
+            print(i.get('data', {}).get('title'))
 
-    except Exception:
+    except ValueError:  # JSON decoding can fail
         print("None")
+    except Exception:  # Catch-all for any other exceptions
+        print("None")
+
