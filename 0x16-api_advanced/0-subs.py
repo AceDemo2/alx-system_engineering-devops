c22d3069cd34857d8bbe9bacb49cc3802d160ef6
AceDemo2 <lawaladepoju705@gmail.com>
2024-09-02 17:46:51 -0700

    fetch counts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """ return number of subscribers of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about/.json"
     header = {'User-Agent': 'law'}
-    response = requests.get(url)
+    response = requests.get(url, headers=header)
     if response.statues_code != 200:
         return 0
     data = response.json()
